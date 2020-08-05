c8e79ecd0e8247da5fedf8bb1af9c5575cebca73
AlexZhadaev <pvt.vatnik@gmail.com>
2020-08-05 23:35:51 +0300

    test upd

---
+++ C2_Lesson3/GB_Vkontakte_AlexeyZhadaev/FindGroups/FindGroupsCell.swift
@@ -0,0 +1,24 @@
+//
+//  FindGroupsCell.swift
+//  GB_Vkontakte_AlexeyZhadaev
+//
+//  Created by Жадаев Алексей on 05.08.2020.
+//  Copyright © 2020 Жадаев Алексей. All rights reserved.
+//
+
+import UIKit
+
+class FindGroupsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
